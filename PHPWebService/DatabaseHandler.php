e7c9bf35638b3cc18483dd6597b35235da5157dc
elborio <elborio@hotmail.com>
2016-02-15 20:30:06 +0100

    Style update app

--- PHPWebService/DatabaseHandler.php
+++ PHPWebService/DatabaseHandler.php
@@ -17,13 +17,22 @@ class DatabaseHandler
         try {
             $this->DBH = new PDO("mysql:host=$this->servername;dbname:patio_melba;", $this->username, $this->password);
         } catch (PDOException $exception) {
-            echo "Connection failed: " + $exception->getMessage();
+            echo "Connection failed: " . $exception->getMessage();
         }
     }
 
     public function addStreepje($userID, $amount) {
         if ($this->DBH != null) {
-            $statement = "INSERT INTO `patio_melba`.`streeplijst` (`id`, `user_id`, `added_by`, `amount`, `date`) VALUES (NULL, '1', '1', '1', CURRENT_TIMESTAMP);";
+            try {
+                $statement = $this->DBH->prepare("INSERT INTO streeplijst (id, user_id,added_by, amount,date)
+                                              VALUES (NULL, :userid, :userid, :amount, CURRENT_TIMESTAMP)");
+                $statement->bindParam(':userid', $userID);
+                $statement->bindParam(':amount', $amount);
+
+                $statement->execute();
+            } catch (PDOException $exception) {
+                echo "Failed to add streepjes: ".$exception->getMessage();
+            }
         }
 
     }
--- PHPWebService/MelbaService.php
+++ PHPWebService/MelbaService.php
@@ -6,6 +6,22 @@
  * Time: 7:03 PM
  */
 
+$postData = $_GET['do'];
 
+//database handler object.
+$databaseHandler = new DatabaseHandler();
+
+if (isset($postData)) {
+    switch($postData) {
+        case "addStreepje":
+            if (isset($_GET['user']) && is_int($_GET['user']) && $_GET['amount'] && is_int($_GET['amount'])) {
+                $databaseHandler->addStreepje($_GET['user'],$_GET['amount']);
+            }
+            break;
+
+    }
+
+
+}
 
 ?>
